608264430d8d157d94db48a4e6c5013403829661
lipeina <954763163@qq.com>
2019-11-06 23:49:16 +0800

    first

---
+++ 01/02.js
@@ -0,0 +1,6 @@
+console.log(process.argv)
+const playerAction = process.argv[2]
+console.log(playerAction)
+var computerAction
+const random = Math.random()*3
+// rock scissor paper
---
+++ 01/index.js
@@ -0,0 +1,4 @@
+console.log('hello')
+console.log(__filename)
+console.log(__dirname)
+console.log(process)
